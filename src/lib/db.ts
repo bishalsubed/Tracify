657786d68c05872979f6c9b77d4a2fcefdcc117e
bishalsubed <142403372+bishalsubed@users.noreply.github.com>
2025-08-17 13:59:38 +0545

    fixed error

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -6,12 +6,12 @@ import * as schema from '@/db/schema/schema';
 
 let dbInstance: ReturnType<typeof drizzle> | null = null;
 
-export async function connectDB(){
+export function connectDB(){
   if(dbInstance) return dbInstance;
   try {
     const sql = neon(process.env.DATABASE_URL!)
     dbInstance = drizzle(sql, {schema})
-    await dbInstance.execute('select 1');
+    
     return dbInstance;
   } catch (error) {
     console.error('Error connecting to the database:', error);
