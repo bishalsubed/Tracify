422abe816c0830050a16450e36c41ceccf97c0f2
bishalsubed <142403372+bishalsubed@users.noreply.github.com>
2025-08-21 22:39:32 +0545

    typo fixed

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -3,7 +3,7 @@ import { DefaultSession } from "next-auth"
 declare module "next-auth" {
     interface Session {
         user: {
-            id: Number;
+            id: number;
         } & DefaultSession["user"]
     }
 }
